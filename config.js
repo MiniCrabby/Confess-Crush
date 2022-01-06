f368e33e1169afa31ae78a9fa5ae5a8af61452f2
MiniCrabby <93459004+MiniCrabby@users.noreply.github.com>
2022-01-06 11:21:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-const CONFIG = {
-    titleWeb: "Mina 😘",
-    introTitle: 'Mina',
-    introDesc: `You know I love you from the first sight`,
-    btnIntro: 'Oh!',
-    title: 'Do you really love me? 🥰',
-    desc: 'Do you want to be my girlfriend? ',
-    btnYes: 'Yes, i am 😘',
-    btnNo: 'No, I am not 😔',
-    question: 'Do you ready for a date?',
-    btnReply: 'Send this to your friend',
-    reply: 'Yes, I am!',
-    mess: 'I knew it 🥰. Love you much 😘😘',
-    messDesc: 'Lets date right now ok?',
-    btnAccept: 'Ok! <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+
